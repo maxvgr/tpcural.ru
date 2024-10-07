5338fc6585ee3cc0009bfa1dd05c387dad061cda
Max V Grushin <support@ids.spb.ru>
2024-10-07 17:44:38 +0300

    feat: добавлена секция Услуги

---
+++ src/js/layout/page/home.js
@@ -0,0 +1,43 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import {
+  Navigation,
+  Autoplay,
+} from "swiper/modules";
+
+new Swiper(".serviceSwiper", {
+  modules: [Navigation, Autoplay],
+
+  slidesPerView: 4,
+  spaceBetween: -5,
+  // loop: true,
+  speed: 2000,
+
+  breakpoints: {
+    260: {
+      slidesPerView: 1,
+    },
+    360: {
+      slidesPerView: 2,
+    },
+    800: {
+      slidesPerView: 3,
+    },
+    1500: {
+      slidesPerView: 4,
+    },
+  },
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".services .swiper-button-next",
+    prevEl: ".services .swiper-button-prev",
+  },
+});
