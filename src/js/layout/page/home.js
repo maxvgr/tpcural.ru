84432343f5c4aa40ae580833f13181f61fda4ca6
Max V Grushin <support@ids.spb.ru>
2024-10-08 10:40:19 +0300

    feat: добавлена секция Клиенты

--- src/js/layout/page/home.js
+++ src/js/layout/page/home.js
@@ -41,3 +41,37 @@ new Swiper(".serviceSwiper", {
     prevEl: ".services .swiper-button-prev",
   },
 });
+
+new Swiper(".clientsSwiper", {
+  modules: [Navigation, Autoplay],
+
+  slidesPerView: 4,
+  spaceBetween: -5,
+  // loop: true,
+  speed: 2000,
+
+  breakpoints: {
+    260: {
+      slidesPerView: 1,
+    },
+    360: {
+      slidesPerView: 2,
+    },
+    800: {
+      slidesPerView: 3,
+    },
+    1500: {
+      slidesPerView: 4,
+    },
+  },
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".our-clients .swiper-button-next",
+    prevEl: ".our-clients .swiper-button-prev",
+  },
+});
